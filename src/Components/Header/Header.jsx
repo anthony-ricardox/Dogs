87d4366014f6f224abbb4960cd60e4a8981f05e9
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-06-06 17:04:56 -0300

    adicionando img svg e estilizando o header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import Dogs from "../../Assents/dogs.svg?react";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
-      <nav className={styles.conteiner}> 
-        <Link to={'/'} >Home</Link>
-        <Link to={'/login'} >Login | Criar</Link>
-
+    <header className={styles.header}>
+    <nav className={`${styles.nav} ${styles.conteiner}`}>
+        <Link className={styles.logo} to={"/"} aria-label="Dogs | Home">
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to={"/login"}>Login | Criar</Link>
       </nav>
+    </header>
+  );
+};
 
-    </div>
-  )
-}
-
-export default Header
+export default Header;
