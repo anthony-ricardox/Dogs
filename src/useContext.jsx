7969aed369cc5c9283e36e2ff73780305670a5a4
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-06-10 22:20:25 -0300

    vou utilizar useContext para utilizar dados nos componentes sem usar props manual

---
+++ src/useContext.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+export const UserContext = React.useContext
+
+export const UseStorage = () => {
+  return (
+    <UserContext.Provider>
+      
+    </UserContext.Provider>
+  )
+}
+
+export default UseStorage
