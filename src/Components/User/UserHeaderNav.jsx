fdd6701b868daaec5e7e03d2dd98b5bb59bdf784
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-06-25 20:25:34 -0300

    estelizando o HeaderNav, e criando um state que quando tiver em tela de mobile ele apareça os texto dos iconoes svg, senão ele renderiza apenas o o icon svg

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -5,24 +5,26 @@ import MinhasFotos from "../../Assents/feed.svg?react";
 import Estatistica from "../../Assents/estatisticas.svg?react";
 import AdicionarFotos from "../../Assents/adicionar.svg?react";
 import Sair from "../../Assents/sair.svg?react";
+import styles from './UserHeaderNav.module.css'
 
 const UserHeaderNav = () => {
   const { useLogout } = React.useContext(UserContext);
+  const [mobile, setMobile] = React.useState(null)
   return (
-    <nav>
+    <nav className={styles.nav}>
       <NavLink to="/conta">
-        <MinhasFotos /> Minhas Fotos
+        <MinhasFotos /> {mobile && 'Minhas Fotos'}
       </NavLink>
 
       <NavLink to="/conta/estatisca">
-        <Estatistica /> Estatística
+        <Estatistica /> {mobile && 'Estatística'}
       </NavLink>
 
       <NavLink to="/conta/postar">
-        <AdicionarFotos /> Adicionar Foto
+        <AdicionarFotos /> {mobile && 'Adicionar Foto'} 
       </NavLink>
       <button onClick={useLogout}>
-        <Sair /> Sair
+        <Sair /> {mobile && 'Sair'}
       </button>
     </nav>
   );
