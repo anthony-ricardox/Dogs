1cd5c5a3d3183bccc33750472d3df71828030a2d
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-06-25 19:54:45 -0300

    removido o botao de sair do header ao lado do nome, e adicionado o botão de sair na tela HeaderNav

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import { UserContext } from "../../useContext";
 
 const UserHeaderNav = () => {
+  const {useLogout} = React.useContext(UserContext)
   return (
     <nav>
       <NavLink to="/conta">Minhas Fotos</NavLink>
       <NavLink to="/conta/estatisca" >Estatística </NavLink>
       <NavLink to="/conta/postar">Adicionar Foto</NavLink>
-      <button>Sair</button>
+      <button onClick={useLogout}>Sair</button>
     </nav>
   );
 };
