0dcda35d8cb25d670e28b4111d138843f48d72b1
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-06-25 21:27:09 -0300

    fix: aparecia no console um fetch toda vez que utilizava o router , usenavigate, no usecontext tiver remover o usenavigate do effect para parar de renderizar toda vez que ativasse a função, criei uma função para utilizar a função Logout e utilizar o navigate para o login

--- src/Components/User/UserHeaderNav.jsx
+++ src/Components/User/UserHeaderNav.jsx
@@ -1,30 +1,36 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { UserContext } from "../../useContext";
 import MinhasFotos from "../../Assents/feed.svg?react";
 import Estatistica from "../../Assents/estatisticas.svg?react";
 import AdicionarFotos from "../../Assents/adicionar.svg?react";
 import Sair from "../../Assents/sair.svg?react";
-import styles from './UserHeaderNav.module.css'
+import styles from "./UserHeaderNav.module.css";
 
 const UserHeaderNav = () => {
   const { useLogout } = React.useContext(UserContext);
-  const [mobile, setMobile] = React.useState(null)
+  const [mobile, setMobile] = React.useState(null);
+  const navigate = useNavigate();
+  function handleLogout() {
+    useLogout();
+    navigate("/login");
+  }
+
   return (
     <nav className={styles.nav}>
       <NavLink to="/conta">
-        <MinhasFotos /> {mobile && 'Minhas Fotos'}
+        <MinhasFotos /> {mobile && "Minhas Fotos"}
       </NavLink>
 
       <NavLink to="/conta/estatisca">
-        <Estatistica /> {mobile && 'Estatística'}
+        <Estatistica /> {mobile && "Estatística"}
       </NavLink>
 
       <NavLink to="/conta/postar">
-        <AdicionarFotos /> {mobile && 'Adicionar Foto'} 
+        <AdicionarFotos /> {mobile && "Adicionar Foto"}
       </NavLink>
-      <button onClick={useLogout}>
-        <Sair /> {mobile && 'Sair'}
+      <button onClick={handleLogout}>
+        <Sair /> {mobile && "Sair"}
       </button>
     </nav>
   );
