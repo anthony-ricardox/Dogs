0efe20ff403f368efcf3b2aa2bda7fcf6531887b
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-01 17:15:50 -0300

    feat: componente de Post de foto do usuario, adicionando inputs e button para preenhcer oque vai ser postado

--- src/Components/User/UserPhotoPost.jsx
+++ src/Components/User/UserPhotoPost.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import styles from './UserPhotoPost.module.css'
+import Input from '../Forms/Input'
+import Button from '../Forms/Button'
 const UserPhotoPost = () => {
+
+  function handleSubmit(e){  
+    e.preventDefault()
+  }
+
+  function handleImgChange(){ 
+
+  }
   return (
-    <div>
-      Postar Foto
-    </div>
+    <section className={`${styles.photoPost} animeLeft `}>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nome" type="text" name="nome" />
+        <Input label="Peso" type="text" name="peso"/>
+        <Input label="Idade" type="text" name="idade"/>
+        <input type="file" name="img" id="img" onChange={handleImgChange}/>
+        
+
+
+        <Button>Enviar</Button>
+      </form>
+    </section>
   )
 }
 
