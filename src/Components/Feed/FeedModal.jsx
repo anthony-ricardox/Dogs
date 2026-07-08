769fc70876a9011aa41cd9c97bb1519e4461691c
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-07 22:26:36 -0300

    feat: configuração do modal adicionado evento de click nas photo que abrira um modal que contera o photo e  conteudo , criado um get de photo na api , para buscar as photo do feed

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
+import styles from './FeedModal.module.css'
+import userFetch from '../../Hooks/userFetch'
+import { PHOTO_GET } from '../../api'
 
-const FeedModal = () => {
+const FeedModal = ({photo}) => {
+  const {data, error, loading, request} = userFetch()
+
+  React.useEffect(()=> {  
+    const {url, options} = PHOTO_GET(photo.id)
+    request(url, options)
+  },[photo, request])
   return (
-    <div>
-      
+    <div className={styles.modal}>
+      <img src={photo.src} alt="" />
     </div>
   )
 }
