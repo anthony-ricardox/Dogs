c05d17df906dac858da224d5d12fc1350942a8ec
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-07 22:33:39 -0300

    feat: exibir loading e erro antes de renderizar conteúdo quando data estiver disponível

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import styles from './FeedModal.module.css'
-import userFetch from '../../Hooks/userFetch'
-import { PHOTO_GET } from '../../api'
+import React from "react";
+import styles from "./FeedModal.module.css";
+import userFetch from "../../Hooks/userFetch";
+import { PHOTO_GET } from "../../api";
+import Error from "../Help/Error";
+import Loading from "../Help/Loading";
 
-const FeedModal = ({photo}) => {
-  const {data, error, loading, request} = userFetch()
+const FeedModal = ({ photo }) => {
+  const { data, error, loading, request } = userFetch();
 
-  React.useEffect(()=> {  
-    const {url, options} = PHOTO_GET(photo.id)
-    request(url, options)
-  },[photo, request])
+  React.useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
   return (
     <div className={styles.modal}>
-      <img src={photo.src} alt="" />
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent  data={data}/>}
     </div>
-  )
-}
+  );
+};
 
-export default FeedModal
+export default FeedModal;
