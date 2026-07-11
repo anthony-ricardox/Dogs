01b5265cc963e0f2f2d643d2860427a119221272
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-10 23:31:46 -0300

    feat(photo/comments): ativar envio de comentários
    Corrige onChange em src/Components/Photo/PhotoCommentsForm.jsx (usa target.value); corrige COMMENT_POST em src/api.jsx (JSON.stringify, Authorization Bearer); padroniza método POST.

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -1,23 +1,37 @@
 import React from "react";
-import Enviar from "../../Assents/enviar.svg?react"
+import Enviar from "../../Assents/enviar.svg?react";
+import userFetch from "../../Hooks/userFetch";
+import {COMMENT_POST} from '../../api'
 
 const PhotoCommentsForm = ({ id }) => {
   const [comment, setComment] = React.useState("");
+  const { error, request } = userFetch();
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const {url, options} = COMMENT_POST( id, {comment})
+    await request(url, options);
+    console.log(comment)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <textarea
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => {
-          setComment(comment.value);
-        }} 
+          setComment(target.value);
+        }}
       />
-      <button> <Enviar/> </button>
+      <button>
+        {" "}
+        <Enviar />{" "}
+        
+      </button>
+      
     </form>
   );
 };
 
 export default PhotoCommentsForm;
-''
