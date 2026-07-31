0d12629904a0db713920bf8a1c8cfcdbc74a4a92
Anthony Ricardo <147872743+anthony-ricardox@users.noreply.github.com>
2026-07-30 22:00:40 -0300

    desabilitando o botao de delete quando tiver deletando, e fazendo um loading quando tiver deletando

--- src/Components/Photo/PhotoDelete.jsx
+++ src/Components/Photo/PhotoDelete.jsx
@@ -1,23 +1,33 @@
-import React from 'react'
-import styles from './PhotoDelete.module.css'
-import userFetch from '../../Hooks/userFetch'
-import { PHOTO_DELETE } from '../../api'
+import React from "react";
+import styles from "./PhotoDelete.module.css";
+import userFetch from "../../Hooks/userFetch";
+import { PHOTO_DELETE } from "../../api";
 
-const PhotoDelete = ({id}) => {
-    const {loading, request} = userFetch()
+const PhotoDelete = ({ id }) => {
+  const { loading, request } = userFetch();
 
-   async function handleClick (event) {   
-        event.preventDefault()
-        const {url, options} = PHOTO_DELETE(id)
-        const {response} = await request(url, options)
-        if( response.ok) window.location.reload()
+  async function handleClick() {
+    const confirm = window.confirm("Tem certeza que deseja deletar? ");
+    if (confirm) {
+      const { url, options } = PHOTO_DELETE(id);
+      const { response } = await request(url, options);
+      if (response.ok) window.location.reload();
     }
+  }
 
   return (
     <div>
-        <button onClick={handleClick} className={styles.delete}> Deletar</button>
+      {loading ? (
+        <button disabled className={styles.delete}>
+          Deletando...
+        </button>
+      ) : (
+        <button onClick={handleClick} className={styles.delete}>
+          Deletar
+        </button>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PhotoDelete
+export default PhotoDelete;
